d1dd6577434b332816188d5d26a7b34b9492467e
david Cormier <david.cormier@savoirfairelinux.com>
2013-10-03 16:25:14 -0400

    Add forecast computation

--- stock_forecast/report/stock_forecast.py
+++ stock_forecast/report/stock_forecast.py
@@ -1,5 +1,6 @@
 from openerp import tools
 from openerp.osv import osv, fields
+from datetime import datetime, timedelta
 
 class stock_forecast(osv.osv):
     _name = "stock.forecast"
@@ -18,5 +19,43 @@ class stock_forecast(osv.osv):
                 to_char(current_date, 'YYYY') as test
         );
         """)
+
+    def get_stock_forecast(self, cr, uid, context=None):
+        product_id = context['product_id']
+        import pdb; pdb.set_trace()
+        self.pool.get('stock_move').search(cr, uid, [('product_id', '=', product_id)])
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='tree', context=None, toolbar=False, submenu=False):
+        result = super(stock_forecast, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar, submenu=submenu)
+        from datetime import datetime
+
+        forecast_context = {'product_id':5,
+                            'datetime': datetime.now()}
+        
+        
+        today = datetime.now()
+        for product_id in range(5,6):
+            for day in range(14):
+                forecast_context = {'product_id': product_id,
+                                    'datetime': today + timedelta(days=day) }
+                forecast_for_day = self.get_stock_forecast(cr, uid, context=forecast_context)
+                print "FORECAST: %s" % forecast_for_day
+                                
+
+        if view_type == 'form':
+            result['arch'] = '''
+
+        <form string="Model" version="7.0">
+          <table>
+
+          </table>
+        </form>
+
+
+            '''
+        return result
+
+
+        
         
 stock_forecast()
