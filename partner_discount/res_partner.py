ee6820e23a18c8e8fd99d1d589df69a62571dd83
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-11-01 11:47:03 -0400

    [ADD] partner_discount which adds a discount percentage to partner

---
+++ partner_discount/res_partner.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp.osv import orm, fields
+
+
+class res_partner(orm.Model):
+    """ Inherits partner and adds discount percentage """
+    _inherit = 'res.partner'
+    _columns = {
+        'discount': fields.float('Discount', help="For percent enter a ratio between 0-1."),
+    }
+    _defaults = {'discount': 0.0}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
