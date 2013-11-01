5367b9b72decc026fcd8ef4cddf4ea73df8b1b25
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-11-01 11:45:17 -0400

    [FIX] stock_move_autolot: fixed typos

--- stock_move_autolot/__openerp__.py
+++ stock_move_autolot/__openerp__.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    This module copyright (C) 2013 Savoir-faire Linux
@@ -17,6 +18,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+###############################################################################
 
 {
     'name': 'Auto-Consume lot',
@@ -24,7 +26,7 @@
     'author': 'Savoir-faire Linux',
     'maintainer': 'Savoir-faire Linux',
     'website': 'http://www.savoirfairelinux.com',
-    'caterogy': 'Manufacturing',
+    'category': 'Manufacturing',
     'description': """\
 Auto lot assignment when consuming product.
 """,
