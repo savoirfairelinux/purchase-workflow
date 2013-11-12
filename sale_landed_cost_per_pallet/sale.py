0e7a4569388b700d647671eb9a32a94cc16f9399
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-11-12 13:56:25 -0500

    [FIX] Using right field name for product quantity

--- sale_landed_cost_per_pallet/sale.py
+++ sale_landed_cost_per_pallet/sale.py
@@ -69,9 +69,9 @@ class sale_order_line(orm.Model):
     _columns = {
         'nb_pallets': fields.integer('Pallets', required=True),
         'nb_crates_per_pallet': fields.integer('Crates per pallet', required=True),
-        'product_qty': fields.function(_product_quantity,
-                                       string="Quantity",
-                                       type='float'),
+        'product_uom_qty': fields.function(_product_quantity,
+                                           string="Quantity",
+                                           type='float'),
         'landing_costs_order': fields.function(_landing_cost_order,
                                                digits_compute=dp.get_precision('Account'),
                                                string='Landing Costs from Order'),
