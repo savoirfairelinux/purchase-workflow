bbb795f261a6b7d65a0ba6ce1722ed9dd545b7e2
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2015-03-30 14:23:57 -0400

    [FIX] Fix log assignement for the case where we have the quantity distributed equally between several lots for the same product

--- purchase_lot_tracking/stock.py
+++ purchase_lot_tracking/stock.py
@@ -22,12 +22,17 @@
 from openerp.osv import orm, fields
 from openerp.tools.translate import _
 
+
 class stock_production_lot(orm.Model):
 
     _inherit = 'stock.production.lot'
 
     _columns = {
-        'account_analytic_id': fields.many2one('account.analytic.account', 'Analytic Account', required=False)
+        'account_analytic_id': fields.many2one(
+            'account.analytic.account',
+            'Analytic Account',
+            required=False
+        )
     }
 
 
@@ -39,9 +44,8 @@ class stock_invoice_onshipping(orm.TransientModel):
         """Retrieves all different move lines from the stock picking
         """
         move_lines = []
-        for pick in self.pool.get('stock.picking').browse(cr, uid,
-                                                          context['active_ids'],
-                                                          context=context):
+        for pick in self.pool.get('stock.picking').browse(
+                cr, uid, context['active_ids'], context=context):
             move_lines += pick.move_lines
 
         return move_lines
@@ -51,8 +55,8 @@ class stock_invoice_onshipping(orm.TransientModel):
 
            expects invoice_id to be passed in context
         """
-        invoice = self.pool.get('account.invoice')\
-                           .browse(cr,uid, context['invoice_id'])
+        invoice = self.pool.get('account.invoice').browse(
+            cr, uid, context['invoice_id'])
         return invoice.invoice_line
 
     def _find_matching_move_line(self, invoice_line, move_lines):
@@ -61,12 +65,28 @@ class stock_invoice_onshipping(orm.TransientModel):
            It is considered matching if they have the same product quantity
            and the same product id.
         """
-        matching_move_lines = [line for line in move_lines if
-                               line.product_qty == invoice_line.quantity and
-                               line.product_id.id == invoice_line.product_id.id]
+        cr = invoice_line._cr
+        uid = invoice_line._uid
+        context = invoice_line._context
+        inv_line_pool = self.pool['account.invoice.line']
+        matching_move_lines = [
+            line for line in move_lines if
+            line.product_qty == invoice_line.quantity and
+            line.product_id.id == invoice_line.product_id.id
+        ]
 
-        if matching_move_lines:
+        if len(matching_move_lines) == 1:
             return matching_move_lines[0]
+        elif len(matching_move_lines) > 1:
+            for l in matching_move_lines:
+                query = [
+                    ('id', '!=', invoice_line.id),
+                    ('invoice_id', '=', invoice_line.invoice_id.id),
+                    ('account_analytic_id', '=', l.prodlot_id.account_analytic_id.id)
+                ]
+                if not inv_line_pool.search(cr, uid, query, context=context):
+                    return l
+
         return None
 
     def create_invoice(self, cr, uid, ids, context=None):
@@ -82,13 +102,13 @@ class stock_invoice_onshipping(orm.TransientModel):
             .create_invoice(cr, uid, ids, context=context)
 
         context['invoice_id'] = res.values()[0]
-        
+
         # retrieve move lines of stock pickings
         move_lines = self.retrieve_move_lines(cr, uid, ids, context=context)
 
         # retrieve invoice lines of newly create invoice
         invoice_lines = self._retrieve_invoice_lines(cr, uid, ids,
-                                                    context=context)
+                                                     context=context)
 
         # first iteration, check if matching
         lst_match = []
@@ -110,15 +130,9 @@ class stock_invoice_onshipping(orm.TransientModel):
                 item, origin %s." % (name, matching_move_line.origin))
                 raise orm.except_orm(_("Missed Serial Number!"), msg)
 
-            matching_prod_lot = prod_lot_pool.browse(cr, uid, prod_lot_id.id)
-            matching_account = matching_prod_lot.account_analytic_id.id
+            invoice_line.write(
+                {'account_analytic_id': prod_lot_id.account_analytic_id.id})
 
-            lst_match.append((invoice_line, matching_account))
-
-        # transaction
-        for invoice_line, matching_account in lst_match:
-            invoice_line.write({'account_analytic_id': matching_account})
-        
         return res
 
 
@@ -134,8 +148,10 @@ class stock_picking_out(orm.Model):
         for line in picking.move_lines:
             if line.product_id.track_production and not line.prodlot_id.id:
                 message = ' '.join([
-                        _('Please specify a lot number for all products of type:'),
-                        line.product_id.name_template])
+                    _('Please specify a lot number for all products of type:'),
+                    line.product_id.name_template
+                ])
                 raise orm.except_orm(_('Missing lot number'), message)
-                        
-        return super(stock_picking_out, self).action_process(cr, uid, ids, context)
+
+        return super(stock_picking_out, self).action_process(
+            cr, uid, ids, context)
